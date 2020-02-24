8a696b25167192daa312ed7edcd4946d7e052aa6
vanduc2514 <vanduc2514@gmail.com>
2020-02-24 15:52:52 +0700

    Thêm tính năng vẽ hình tam giác xuôi / ngược

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-function drawTriangle() {
+function drawTriangleUp() {
     let loopCycle = 1;
     let html = "";
     while (loopCycle < 100) {
         html += "<hr width = " + loopCycle + "%>";
         loopCycle++;
+    }
+    document.getElementById('drawResult').innerHTML = html;
+}
+
+function drawTriangleDown() {
+    let loopCycle = 100;
+    let html = "";
+    while (loopCycle > 0) {
+        html += "<hr width = " + loopCycle + "%>";
+        loopCycle--;
     }
     document.getElementById('drawResult').innerHTML = html;
 }
